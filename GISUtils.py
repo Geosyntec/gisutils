11cd96fa220de41a1686ecd738a3154a6ecd3079
R. Dylan Walker <dylan_walker@yahoo.com>
2015-06-16 13:48:00 -0400

    Intialize library

---
+++ GISUtils.py
@@ -0,0 +1,9 @@
+'''
+Geosyntec written tools to perform mundane, tedious tasks for ArcGIS MXDs.
+Contributors:
+Jeffrey Munowitch, Oakland (jmunowitch@geosyntec.com)
+Paul Hobson, Portland (phobson@geosyntec.com)
+R. Dylan Walker, Columbia, MD (rwalker@geosyntec.com)
+'''
+
+import arcpy
