d85b704fe5b96b9e29b8d14f2cf48a6f98b42370
Paul Hobson <pmhobson@gmail.com>
2015-08-12 11:47:19 -0700

    add and start core module
    Added the Extension and OverwriteState context managers and the
    EasyMapDoc class.

--- Mapping/__init__.py
+++ Mapping/__init__.py
@@ -1,1 +1,6 @@
-'''Initialization File so that modules in this directory are accessible to outside modules'''
+"""
+Initialization File so that modules in this
+directory are accessible to outside modules
+
+"""
+from .core import Extension, EasyMapDoc, OverwriteState
