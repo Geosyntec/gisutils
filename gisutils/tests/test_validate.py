52a52e13ad4134533f96f4bbd5dac646def3c75b
Paul Hobson <pmhobson@gmail.com>
2016-07-19 16:55:25 -0700

    add function to validate non-empty lists

--- gisutils/tests/test_validate.py
+++ gisutils/tests/test_validate.py
@@ -15,7 +15,6 @@ from gisutils import validate
     ([1, 2, 3], numpy.array([1, 2, 3])),
     (numpy.array([1, 2, 3]), numpy.array([1, 2, 3])),
     (numpy.array([[1, 2, 3], [4, 5, 6]]), numpy.array([1, 2, 3, 4, 5, 6])),
-
 ])
 def test_is_vector(value, expected):
     result = validate.is_vector(value)
@@ -40,3 +39,26 @@ def test_mpl_axes_with_None():
     fig1, ax1 = validate.mpl_axes(None)
     assert isinstance(ax1, pyplot.Axes)
     assert isinstance(fig1, pyplot.Figure)
+
+
+@pytest.mark.parametrize(('obj', 'expected', 'err'), [
+    ([1, 2, 3], [1, 2, 3], None),
+    (1, [1], None),
+    (None, None, ValueError),
+    ([], None, ValueError),
+])
+def test_non_empty_list_default(obj, expected, err):
+    if err is None:
+        result = validate.non_empty_list(obj)
+        assert result == expected
+    else:
+        with pytest.raises(err):
+            validate.non_empty_list(obj)
+
+
+@pytest.mark.parametrize('obj', [None, []])
+@pytest.mark.parametrize('on_fail', ['empty', 'create'])
+def test_non_empty_list_create(obj, on_fail):
+    result = validate.non_empty_list(obj, on_fail=on_fail)
+    assert result == []
+
--- gisutils/validate.py
+++ gisutils/validate.py
@@ -43,3 +43,66 @@ def mpl_axes(ax, fallback='new'):
         raise ValueError(msg)
 
     return fig, ax
+
+
+def non_empty_list(list_obj, msg=None, on_fail='error'):
+    """
+    Validates a list as having at least one element.
+
+    Parameters
+    ----------
+    list_obj : list or scalar
+        The object that needs to be validated as a non-empty list. If
+        a scalar value is provided, that value is placed inside a new
+        list.
+    msg : str, optional
+        Custom error message to be raised.
+    on_fail : str, optional
+        Desired behavior when ``list_obj`` cannot be validated. Valid
+        values are `"error"` and `"create"`. The former raises an error.
+        The later returns an empty string.
+
+    Raises
+    ------
+    ValueError
+        A `ValueError` is raised when `list_obj` is an empty list or
+        `None` and on_fail is set to `'error'`, which is the default.
+
+    Returns
+    -------
+    validated : list
+        The validated list object.
+
+    Examples
+    --------
+    >>> from gisutils import validate
+    >>> validate.non_empty_list([1, 2, 3])
+    [1, 2, 3]
+
+    >>> try:
+    ...     validate.non_empty_list([])
+    ... except:
+    ...     print("List was empty")
+    List was empty
+
+    >>> validate.non_empty_list([], on_fail='create')
+    []
+
+    >>> validate.non_empty_list(2)
+    [2]
+
+    """
+
+    if msg is None:
+        msg = "list cannot be empty or None"
+
+    if numpy.isscalar(list_obj):
+        list_obj = [list_obj]
+
+    if list_obj is None or len(list_obj) == 0:
+        if on_fail in ('error', 'raise'):
+            raise ValueError(msg)
+        elif on_fail in ('empty', 'create'):
+            list_obj = []
+
+    return list_obj
