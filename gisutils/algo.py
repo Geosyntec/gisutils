82594e20b080705b2733e255088c31d739e488a9
Austin Orr <austinorr@users.noreply.github.com>
2016-09-07 18:34:30 -0700

    Added algo.py with average slope function, unit tests (#16)
    Added algo.py with average slope function, unit tests

---
+++ gisutils/algo.py
@@ -0,0 +1,40 @@
+from gisutils import raster
+
+def average_slope(gdf, dem, dem_affine):
+    """
+    Computes the average slope between the first and last coordinates of 
+    a line using the elevations from a reference Digital Elevation Map.
+
+    Output DataFrame has new column `average_slope` containing the result
+    of the slope calculation (float).
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame
+    dem : array
+    dem_affine : affine.Affine
+    
+    
+    Returns
+    -------
+    GeoDataFrame
+    
+    """
+    
+    length = gdf['geometry'].length
+    x1 = gdf['geometry'].apply(lambda g: g.coords[0][0])
+    y1 = gdf['geometry'].apply(lambda g: g.coords[0][1])
+
+    x2 = gdf['geometry'].apply(lambda g: g.coords[-1][0])
+    y2 = gdf['geometry'].apply(lambda g: g.coords[-1][1])
+    
+    r1, c1 = raster.xy_to_rowcol(x1, y1, dem_affine)
+    z1 = dem[r1,c1]
+    
+    r2, c2 = raster.xy_to_rowcol(x2, y2, dem_affine)
+    z2 = dem[r2,c2]
+    
+
+    slope = (z2 - z1)/ length
+    return gdf.assign(avg_slope=slope)
+    
---
+++ gisutils/tests/test_algo.py
@@ -0,0 +1,33 @@
+from shapely import geometry
+import geopandas
+import numpy
+from affine import Affine
+from gisutils import algo
+
+def test_average_slope():
+    
+    _lines = [
+        geometry.LineString(coordinates = [(0, 5), (10, 5)]), # across the center (slope = 1)
+        geometry.LineString(coordinates = [(5, 0), (5, 10)]), # up the center (slope = 0)
+        geometry.LineString(coordinates = [(0, 0), (10, 10)]), # corner to corner (slope ~0.707)
+        geometry.LineString(coordinates = [(0, 0), (0, 10), (10, 10)]), # corner to corner to corner (slope = 0.5)
+        geometry.LineString(coordinates = [(0, 0), (5, 5), (5, 0), (0, 0)]), # loops back to start (slope = 0)
+    ]
+
+    lines = geopandas.GeoDataFrame(geometry = _lines)
+    
+    expected = [
+        1.000000,
+        0.000000,
+        0.707107,
+        0.500000,
+        0.000000,
+    ]
+    
+    hill = numpy.mgrid[:11, :11][1]
+    
+    trans = Affine.translation(0, 10) * Affine.rotation(0) * Affine.scale(1, -1)
+    
+    result = algo.average_slope(lines, hill, trans)
+
+    assert numpy.allclose(result.avg_slope, expected)
