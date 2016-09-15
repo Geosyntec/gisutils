23c20e132736dea7fae54596d4dad753340fb785
Paul Hobson <pmhobson@gmail.com>
2016-09-15 15:36:12 -0700

    slope function just returns a series now

--- gisutils/tests/test_algo.py
+++ gisutils/tests/test_algo.py
@@ -4,18 +4,21 @@ import numpy
 from affine import Affine
 from gisutils import algo
 
+
+import pandas.util.testing as pdtest
+
+
 def test_average_slope():
-    
     _lines = [
-        geometry.LineString(coordinates = [(0, 5), (10, 5)]), # across the center (slope = 1)
-        geometry.LineString(coordinates = [(5, 0), (5, 10)]), # up the center (slope = 0)
-        geometry.LineString(coordinates = [(0, 0), (10, 10)]), # corner to corner (slope ~0.707)
-        geometry.LineString(coordinates = [(0, 0), (0, 10), (10, 10)]), # corner to corner to corner (slope = 0.5)
-        geometry.LineString(coordinates = [(0, 0), (5, 5), (5, 0), (0, 0)]), # loops back to start (slope = 0)
+        geometry.LineString(coordinates=[(0, 5), (10, 5)]),
+        geometry.LineString(coordinates=[(5, 0), (5, 10)]),
+        geometry.LineString(coordinates=[(0, 0), (10, 10)]),
+        geometry.LineString(coordinates=[(0, 0), (0, 10), (10, 10)]),
+        geometry.LineString(coordinates=[(0, 0), (5, 5), (5, 0), (0, 0)]),
     ]
 
-    lines = geopandas.GeoDataFrame(geometry = _lines)
-    
+    lines = geopandas.GeoDataFrame(geometry=_lines)
+
     expected = [
         1.000000,
         0.000000,
@@ -23,11 +26,9 @@ def test_average_slope():
         0.500000,
         0.000000,
     ]
-    
+
     hill = numpy.mgrid[:11, :11][1]
-    
     trans = Affine.translation(0, 10) * Affine.rotation(0) * Affine.scale(1, -1)
-    
     result = algo.average_slope(lines, hill, trans)
 
-    assert numpy.allclose(result.avg_slope, expected)
+    assert pdtest.assert_series_equal(result, expected)
